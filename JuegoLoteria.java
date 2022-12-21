8cab61281e74241e85f8ca3199d8b40864e4c5a9
Luciano Martínez <luchomartinezcaef@gmail.com>
2022-12-21 21:48:21 +0100

    JuegoLoteríaConJOptionPane

--- JuegoLoteria.java
+++ JuegoLoteria.java
@@ -1,28 +1,41 @@
+import java.util.Arrays;
 import java.util.Random;
+import javax.swing.JOptionPane;
 public class JuegoLoteria {
 
 
     public static void main(String[] args) {
 
+
         int [] boleto = new int[6];
         Random azar = new Random();
         int c=0;
-
+        String boletoStr;
 
         do{
-            int valor=azar.nextInt(49)+1;
+             int valor=azar.nextInt(49)+1;
             boolean repetido= false;
 
             for(int i=0;i<c;i++){
                 if(boleto[i]==valor) repetido=true;
             }
 
-            if (!repetido){
+            if (repetido==false){
+                boleto[c] = valor;
 
+                c++;
             }
-            c++;
+
 
         }while(c<6);
 
+        int reintegro = azar.nextInt(5)+1;
+
+        boletoStr = Arrays.toString(boleto);
+
+        JOptionPane.showMessageDialog(null, "Tu número de la primitiva es:\n"+"\n"+boletoStr+"\n"+"\nReintegro: "+reintegro);
     }
+
+
 }
+
